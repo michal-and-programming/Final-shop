884bba3470e4fe637658e3d812fc6927d4e60ffa
Michał Puchała <michalpuchala86@gmail.com>
2026-03-17 20:50:37 +0100

    add route product controller product connect mongo

---
+++ backend/models/Product.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  images: [String],
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectToDB from "./db.js";
+import productRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 
@@ -10,10 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+connectToDB();
 
 app.get("/", (req, res) => {
   res.send("Serwer backendowy działa!");
 });
+
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Serwer działa na http://localhost:${PORT}`);
