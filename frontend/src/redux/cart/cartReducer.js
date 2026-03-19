2fe79e5bcdea4c19981c586d94b94ed333201c1e
Michał Puchała <michalpuchala86@gmail.com>
2026-03-19 19:06:00 +0100

    add localStorage

--- frontend/src/redux/cart/cartReducer.js
+++ frontend/src/redux/cart/cartReducer.js
@@ -1,7 +1,20 @@
 import { ADD_TO_CART, SET_QUANTITY, SET_INFO, REMOVE } from "./cart.types";
 
+const loadCartFromStorage = () => {
+  try {
+    const data = localStorage.getItem("cart");
+    return data ? JSON.parse(data) : [];
+  } catch {
+    return [];
+  }
+};
+
+const saveCartToStorage = (cart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
 const initialState = {
-  cart: []
+  cart: loadCartFromStorage()
 };
 
 const cartReducer = (state = initialState, action) => {
@@ -10,51 +23,62 @@ const cartReducer = (state = initialState, action) => {
 
       const existingProduct = state.cart.find(item => item.id === action.payload.id);
 
+      let updatedCart;
+
       if(existingProduct){
-        return(
-          {...state,
-            cart: state.cart.map(
-              item => item.id === action.payload.id
-              ? {...item, quantity: item.quantity + action.payload.quantity}
-              : item
-            )
-          }
-        )
+        updatedCart = state.cart.map(item =>
+          item.id === action.payload.id
+          ? { ...item, quantity: item.quantity + action.payload.quantity }
+          : item
+        );
+      } else {
+        updatedCart = [...state.cart, action.payload];
       }
+      saveCartToStorage(updatedCart);
 
       return(
-        {...state, cart: [...state.cart, action.payload]}
+        {...state, cart: updatedCart}
       )
     }
 
-    case SET_QUANTITY:
-      return(
-        {...state, 
-          cart: state.cart.map(
-            item => item.id === action.payload.id
-            ? {...item, quantity: action.payload.quantity}
-            : item
-          )
-        }
-      );
+    case SET_QUANTITY:{ 
+      const updatedCart = state.cart.map(
+        item => item.id === action.payload.id
+        ? {...item, quantity: action.payload.quantity}
+        : item
+      )
+      saveCartToStorage(updatedCart);
 
-    case SET_INFO:
       return(
-        {...state, 
-          cart: state.cart.map(
-            item => item.id === action.payload.id
-            ? {...item, info: action.payload.info}
-            : item
-          )
-        }
-      );
+        {...state, cart: updatedCart}
+      )
+    }
 
-    case REMOVE:
-      return(
-        {...state, cart: state.cart.filter(item => item.id !== action.payload.id)}
-      );
+  case SET_INFO: {
+    const updatedCart = state.cart.map(item =>
+      item.id === action.payload.id
+      ? { ...item, info: action.payload.info }
+      : item
+    );
 
-    default: return state;
+    saveCartToStorage(updatedCart);
+
+    return( 
+      {...state, cart: updatedCart}
+    );
+  }
+
+  case REMOVE: {
+    const updatedCart = state.cart.filter(item => item.id !== action.payload.id);
+
+    saveCartToStorage(updatedCart);
+
+    return( 
+      {...state, ncart: updatedCart}
+    )
+  }
+
+  default: return state;
   }
 }
 
