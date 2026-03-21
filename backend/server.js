626f164813b3eea1f559dd9bdd66aaed2a54c4ab
Michał Puchała <michalpuchala86@gmail.com>
2026-03-21 15:44:43 +0100

    change server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import connectToDB from "./db.js";
 import productRoutes from "./routes/product.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -11,19 +13,26 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-
-connectToDB();
-
-app.get("/", (req, res) => {
-  res.send("Serwer backendowy działa!");
-});
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Serwer działa na http://localhost:${PORT}`);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+connectToDB().then(() => {
+  console.log("Connected to DB");
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
