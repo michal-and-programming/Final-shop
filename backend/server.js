f0bcff4946613c23abb5f0b596e164bb282d4cc0
Michał Puchała <michalpuchala86@gmail.com>
2026-03-25 16:22:36 +0100

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/orders", orderRoutes);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
